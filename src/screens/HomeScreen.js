0e0eca64078aabc01880b0e26e83797da5fd8863
Ho Trong Son <jason@Hos-MacBook-Pro.local>
2022-03-28 03:43:28 +0700

    [trongson] upate UI Home Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,91 +1,185 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text, SafeAreaView, StatusBar, FlatList } from 'react-native'
-import { axiosInstance } from '../utils/axiosInstance'
-import { COLORS } from '../constants/theme'
-import { FormButton } from '../components'
-import { useDispatch, useSelector } from 'react-redux'
-import { quizzesSelector } from '../redux/selector'
-import { addQuizzes } from '../redux/actions'
-import { QuizItem } from '../components'
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { axiosInstance } from "../utils/axiosInstance";
+import { COLORS } from "../constants/theme";
+import { FormButton } from "../components";
+import { useDispatch, useSelector } from "react-redux";
+import { quizzesSelector } from "../redux/selector";
+import { addQuizzes } from "../redux/actions";
+import { QuizItem } from "../components";
+import avatar from "../../assets/images/avatar.jpeg";
+import banner from "../../assets/images/banner.png";
+
+const listOption = ["Profile", "Rank", "Settings", "Logout"];
+
+const ModalUser = () => {
+  return (
+    <View
+      style={{
+        position: "absolute",
+        top: 50,
+        right: 10,
+        backgroundColor: COLORS.white,
+        paddingHorizontal: 30,
+        elevation: 3,
+        borderRadius: 10,
+        paddingVertical: 20,
+      }}
+    >
+      {listOption.map((option, index) => (
+        <View
+          key={index}
+          style={{
+            padding: 10,
+            borderBottomWidth: 1,
+            borderColor: COLORS.border,
+            marginBottom: 10,
+          }}
+        >
+          <Text style={{ fontWeight: "bold" }}>{option}</Text>
+        </View>
+      ))}
+    </View>
+  );
+};
 
 export const HomeScreen = ({ navigation }) => {
-  const quizzes = useSelector(quizzesSelector)
+  const quizzes = useSelector(quizzesSelector);
 
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-  console.log(quizzes)
-  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [openModal, setOpenModal] = useState(false);
+  
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const getAllQuizzes = async () => {
       try {
-        setLoading(true)
-        const { data } = await axiosInstance.get('/quizzes')
-        dispatch(addQuizzes(data))
-        setLoading(false)
+        setLoading(true);
+        const { data } = await axiosInstance.get("/quizzes");
+        dispatch(addQuizzes(data));
+        setLoading(false);
       } catch (error) {
         const message = error?.response
           ? error?.response.data.message
-          : error.message
-        setError(message)
-        setLoading(false)
+          : error.message;
+        setError(message);
+        setLoading(false);
       }
-    }
+    };
     if (quizzes.length < 1) {
-      getAllQuizzes()
+      getAllQuizzes();
     }
-  }, [quizzes.length, dispatch])
+  }, [quizzes.length, dispatch]);
+
   return (
     <SafeAreaView
       style={{
         flex: 1,
         backgroundColor: COLORS.background,
-        position: 'relative',
+        position: "relative",
       }}
+      onPress={() => setOpenModal(false)}
     >
-      <StatusBar backgroundColor={COLORS.white} barStyle={'dark-content'} />
+      <StatusBar backgroundColor={COLORS.white} barStyle={"dark-content"} />
 
+      {/* Navbar */}
       <View
         style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          backgroundColor: COLORS.white,
-          elevation: 4,
-          paddingHorizontal: 20,
+          position: "relative",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingHorizontal: 10,
+          paddingVertical: 10,
         }}
       >
-        <Text style={{ fontSize: 20, color: COLORS.black }}>Quiz App</Text>
-        <Text
-          style={{
-            fontSize: 20,
-            padding: 10,
-            color: COLORS.error,
-          }}
-          // onPress={signOut}
-        >
-          Logout
+        <Text style={{ fontSize: 20, color: COLORS.white, fontWeight: "bold" }}>
+          Quiz App
         </Text>
+        <TouchableOpacity onPress={() => setOpenModal(!openModal)}>
+          <Image
+            source={avatar}
+            style={{ width: 40, height: 40, borderRadius: 50 }}
+          />
+        </TouchableOpacity>
+        {openModal ? <ModalUser /> : null}
       </View>
 
+      {/* Banner */}
+      <View
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          elevation: 1,
+        }}
+      >
+        <Image source={banner} style={{ width: 250, height: 250 }} />
+      </View>
+
+      {/* Body */}
       {!!error ? (
         <>
           {/* Error message */}
           <Text
             style={{
               fontSize: 14,
-              textAlign: 'center',
-              color: 'white',
+              textAlign: "center",
+              color: "white",
               marginBottom: 12,
               padding: 3,
               borderRadius: 3,
-              backgroundColor: 'rgba(255, 0, 67, 0.55)',
+              backgroundColor: "rgba(255, 0, 67, 0.55)",
             }}
           >
             {error}
           </Text>
         </>
       ) : (
-        <>
+        <View
+          style={{
+            backgroundColor: COLORS.white,
+            borderTopLeftRadius: 30,
+            borderTopRightRadius: 30,
+            position: "relative",
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginTop: 3,
+            }}
+          >
+            <View
+              style={{
+                width: 40,
+                height: 5,
+                backgroundColor: COLORS.background,
+                borderRadius: 20,
+              }}
+            ></View>
+          </View>
+          <Text
+            style={{
+              marginVertical: 10,
+              marginLeft: 10,
+              fontSize: 18,
+              fontWeight: "bold",
+            }}
+          >
+            Choose quiz
+          </Text>
           {/* Quizzes list */}
           <FlatList
             data={quizzes}
@@ -93,26 +187,61 @@ export const HomeScreen = ({ navigation }) => {
             style={{
               paddingVertical: 20,
             }}
-            renderItem={({ item }) => (
-              <QuizItem title={item.title} description={item.description} />
+            renderItem={({ item, index }) => (
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  paddingHorizontal: 10,
+                }}
+              >
+                {index < 9 ? (
+                  <Text
+                    style={{
+                      fontSize: 40,
+                      fontWeight: "bold",
+                      color: "#E4E7F4",
+                    }}
+                  >
+                    {"0" + (index + 1)}
+                  </Text>
+                ) : (
+                  <Text
+                    style={{
+                      fontSize: 40,
+                      fontWeight: "bold",
+                      color: "#E4E7F4",
+                    }}
+                  >
+                    {index + 1}
+                  </Text>
+                )}
+                <View style={{ flex: 1 }}>
+                  <QuizItem quiz={item} navigation={navigation} />
+                </View>
+              </View>
             )}
-            keyExtractor={item => item.id}
+            keyExtractor={(item) => item.id}
           />
-        </>
+          {/* Button */}
+          <AntDesign
+            style={{
+              position: "absolute",
+              top: 10,
+              right: -20,
+              borderRadius: 50,
+              paddingHorizontal: 30,
+            }}
+            name="pluscircleo"
+            size={35}
+            color={COLORS.play}
+            onPress={() => {
+              navigation.navigate("CreateQuizScreen");
+            }}
+          />
+        </View>
       )}
-
-      {/* Button */}
-      <FormButton
-        labelText='Create Quiz'
-        style={{
-          position: 'absolute',
-          bottom: 20,
-          right: 20,
-          borderRadius: 50,
-          paddingHorizontal: 30,
-        }}
-        handleOnPress={() => navigation.navigate('CreateQuizScreen')}
-      />
     </SafeAreaView>
-  )
-}
+  );
+};
