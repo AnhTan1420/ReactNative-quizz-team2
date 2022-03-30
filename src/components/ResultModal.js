b4127856d793f41562286e5a2688dd9dcdaea4b4
Tonny <nguyenxuantung473@gmail.com>
2022-03-30 12:07:31 +0700

    [tungnx] Update Result modal

--- src/components/ResultModal.js
+++ src/components/ResultModal.js
@@ -1,118 +1,101 @@
-import React from 'react'
-import { View, Text, Modal, TouchableOpacity } from 'react-native'
-import { COLORS } from '../constants/theme'
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import React from "react";
+import { View, Text, Modal, TouchableOpacity, StyleSheet } from "react-native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 export const ResultModal = ({
-	isModalVisible,
-	correctCount,
-	incorrectCount,
-	totalCount,
-	handleOnClose,
-	handleRetry,
-	handleHome,
+  isModalVisible,
+  correctCount,
+  incorrectCount,
+  totalCount,
+  handleOnClose,
+  handleRetry,
+  handleHome,
 }) => {
-	return (
-		<Modal
-			animationType={'slide'}
-			transparent={true}
-			visible={isModalVisible}
-			onRequestClose={handleOnClose}
-		>
-			<View
-				style={{
-					flex: 1,
-					backgroundColor: COLORS.black + '90',
-					justifyContent: 'center',
-					alignItems: 'center',
-				}}
-			>
-				<View
-					style={{
-						backgroundColor: COLORS.white,
-						width: '90%',
-						borderRadius: 5,
-						padding: 40,
-						alignItems: 'center',
-					}}
-				>
-					<Text style={{ fontSize: 28, color: COLORS.black }}>Results</Text>
-					<View
-						style={{
-							flexDirection: 'row',
-							alignItems: 'center',
-							justifyContent: 'space-between',
-						}}
-					>
-						<View style={{ alignItems: 'center', padding: 20 }}>
-							<Text style={{ color: COLORS.success, fontSize: 30 }}>
-								{correctCount}
-							</Text>
-							<Text style={{ fontSize: 16 }}>Correct</Text>
-						</View>
-						<View style={{ alignItems: 'center', padding: 20 }}>
-							<Text style={{ color: COLORS.error, fontSize: 30 }}>
-								{incorrectCount}
-							</Text>
-							<Text style={{ fontSize: 16 }}>Incorrect</Text>
-						</View>
-					</View>
-					<Text style={{ opacity: 0.8 }}>
-						{totalCount - (incorrectCount + correctCount)} Unattempted
-					</Text>
+  return (
+    <Modal
+      animationType={"slide"}
+      transparent={true}
+      visible={isModalVisible}
+      onRequestClose={handleOnClose}
+    >
+      <View style={styles.modalBodyContainer}>
+        <View style={styles.modalContentContainer}>
+          <Text style={styles.title}>Results</Text>
+          <View style={styles.result}>
+            <View style={styles.resultTextContainer}>
+              <Text style={[styles.resultText, { color: "#00C851" }]}>
+                {correctCount}
+              </Text>
+              <Text style={{ fontSize: 16 }}>Correct</Text>
+            </View>
+            <View style={styles.resultTextContainer}>
+              <Text style={[styles.resultText, { color: "#ff4444" }]}>
+                {incorrectCount}
+              </Text>
+              <Text style={{ fontSize: 16 }}>Incorrect</Text>
+            </View>
+          </View>
+          <Text style={{ opacity: 0.8 }}>
+            {totalCount - (incorrectCount + correctCount)} Unattempted
+          </Text>
 
-					{/* Try agian */}
-					<TouchableOpacity
-						style={{
-							flexDirection: 'row',
-							alignItems: 'center',
-							justifyContent: 'center',
-							paddingVertical: 10,
-							width: '100%',
-							backgroundColor: COLORS.primary,
-							marginTop: 20,
-							borderRadius: 50,
-						}}
-						onPress={handleRetry}
-					>
-						<MaterialIcons name='replay' style={{ color: COLORS.white }} />
-						<Text
-							style={{
-								textAlign: 'center',
-								color: COLORS.white,
-								marginLeft: 10,
-							}}
-						>
-							Try Again
-						</Text>
-					</TouchableOpacity>
-					{/* Go Home */}
-					<TouchableOpacity
-						style={{
-							flexDirection: 'row',
-							alignItems: 'center',
-							justifyContent: 'center',
-							paddingVertical: 10,
-							width: '100%',
-							backgroundColor: COLORS.primary + '20',
-							marginTop: 20,
-							borderRadius: 50,
-						}}
-						onPress={handleHome}
-					>
-						<MaterialIcons name='home' style={{ color: COLORS.primary }} />
-						<Text
-							style={{
-								textAlign: 'center',
-								color: COLORS.primary,
-								marginLeft: 10,
-							}}
-						>
-							Go Home
-						</Text>
-					</TouchableOpacity>
-				</View>
-			</View>
-		</Modal>
-	)
-}
+          {/* Try agian */}
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: "#4630EB" }]}
+            onPress={handleRetry}
+          >
+            <MaterialIcons name="replay" style={{ color: "#FFFFFF" }} />
+            <Text style={styles.butonText}>Try Again</Text>
+          </TouchableOpacity>
+          {/* Go Home */}
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: "#4630EB" + "20" }]}
+            onPress={handleHome}
+          >
+            <MaterialIcons name="home" style={{ color: "#4630EB" }} />
+            <Text style={[styles.butonText, { color: "#4630EB" }]}>
+              Go Home
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+const styles = StyleSheet.create({
+  modalBodyContainer: {
+    flex: 1,
+    backgroundColor: "#171717" + "90",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContentContainer: {
+    backgroundColor: "#FFFFFF",
+    width: "90%",
+    borderRadius: 5,
+    padding: 40,
+    alignItems: "center",
+  },
+  result: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  resultTextContainer: { alignItems: "center", padding: 20 },
+  resultText: { color: "black", fontSize: 30 },
+  title: { fontSize: 28, color: "#171717" },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 10,
+    width: "100%",
+    marginTop: 20,
+    borderRadius: 50,
+  },
+  butonText: {
+    textAlign: "center",
+    color: "#FFFFFF",
+    marginLeft: 10,
+  },
+});
